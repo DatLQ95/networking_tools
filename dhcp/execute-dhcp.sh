7872f009410f94be3882bee6290a792082b2161f
Dat Luong <d.luong@genexis.eu>
2023-08-18 12:56:09 +0000

    modify dhcp file

--- dhcp/execute-dhcp.sh
+++ dhcp/execute-dhcp.sh
@@ -1,6 +1,7 @@
-cp ~/dhcp/dhcpd.conf /etc/dhcp/
-cp ~/dhcp/isc-dhcp-server /etc/default/
-# rm -f /var/lib/dhcp/dhcpd.leases
+cp ../dhcp/dhcpd.conf /etc/dhcp/
+cp ../dhcp/isc-dhcp-server /etc/default/
+rm -f /var/lib/dhcp/dhcpd.leases
 systemctl stop isc-dhcp-server
 systemctl start isc-dhcp-server
-
+sleep 1
+systemctl status isc-dhcp-server
